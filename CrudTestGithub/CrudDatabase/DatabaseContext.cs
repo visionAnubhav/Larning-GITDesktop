402d0ffef75a993862e6568ad6d5088b32fe8129
visionAnubhav <142965433+visionAnubhav@users.noreply.github.com>
2023-08-23 15:27:14 +0530

    Migration Done
    Migration Time Commite

---
+++ CrudTestGithub/CrudDatabase/DatabaseContext.cs
@@ -0,0 +1,14 @@
+﻿using CrudDatabase.tblCommit;
+using Microsoft.EntityFrameworkCore;
+
+namespace CrudDatabase
+{
+    public class DatabaseContext :DbContext
+    {
+        public DatabaseContext(DbContextOptions<DatabaseContext> options):base(options)
+        {
+            
+        }
+      public DbSet<tblGitCommitInfo> GitCommitTable { get; set; }
+    }
+}
---
+++ CrudTestGithub/CrudDatabase/Modals/tblGitCommitInfo.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrudDatabase.tblCommit
+{
+    public class tblGitCommitInfo
+    {
+        [Key]
+        public int Id { get; set; }
+        public string CommitName { get; set; }
+
+    }
+}
